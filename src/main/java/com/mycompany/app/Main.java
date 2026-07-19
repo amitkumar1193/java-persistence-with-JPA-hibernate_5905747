f87c683d6b457f2536f47aa0a4d1c1eb7722c7c9
amitkumar1193 <amitkumar_86@rocketmail.com>
2026-07-19 07:40:30 +0000

    Implementing Persist

--- src/main/java/com/mycompany/app/Main.java
+++ src/main/java/com/mycompany/app/Main.java
@@ -1,5 +1,7 @@
 package com.mycompany.app;
 
+import com.mycompany.app.entities.Book;
+
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
@@ -9,5 +11,20 @@ public class Main {
     EntityManagerFactory emf = Persistence.createEntityManagerFactory("library_persistence_unit");
 
     EntityManager em = emf.createEntityManager(); // Represent the persistence context
-}
+
+    try {
+      em.getTransaction().begin();
+      Book book = new Book();
+
+      book.setName("my book");
+      book.setIsbn("123-456");
+      em.persist(book);
+
+      em.getTransaction().commit();
+    } finally
+
+    {
+      em.close();
+    }
+  }
 }
