d21687a1faa4283531b13ac7ffac2b2b415b8623
amitkumar1193 <amitkumar_86@rocketmail.com>
2026-07-19 07:26:22 +0000

    Exercise changes

--- src/main/java/com/mycompany/app/entities/Book.java
+++ src/main/java/com/mycompany/app/entities/Book.java
@@ -2,12 +2,17 @@ package com.mycompany.app.entities;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "book")
 public class Book {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   @Column(name = "book_id")
   private int id;
 
